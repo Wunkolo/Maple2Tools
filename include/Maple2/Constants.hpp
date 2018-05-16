1316c6444a8057e62a0d8f0e47d54913e128d801
Wunkolo <wunkolo@gmail.com>
2018-05-16 13:53:26 -0700

    Fix header magic namespace

--- include/Maple2/Constants.hpp
+++ include/Maple2/Constants.hpp
@@ -4,8 +4,6 @@
 namespace Maple2
 {
 
-namespace {
-
 constexpr std::uint32_t MakeMagic(
 	char Byte1,	char Byte2,
 	char Byte3,	char Byte4
@@ -19,13 +17,12 @@ constexpr std::uint32_t MakeMagic(
 	);
 }
 
-}
 enum class Magic : std::uint32_t
 {
-	MS2F = ::MakeMagic( 'M', 'S', '2', 'F' ),
-	NS2F = ::MakeMagic( 'N', 'S', '2', 'F' ),
-	OS2F = ::MakeMagic( 'O', 'S', '2', 'F' ),
-	PS2F = ::MakeMagic( 'P', 'S', '2', 'F' )
+	MS2F = MakeMagic( 'M', 'S', '2', 'F' ),
+	NS2F = MakeMagic( 'N', 'S', '2', 'F' ),
+	OS2F = MakeMagic( 'O', 'S', '2', 'F' ),
+	PS2F = MakeMagic( 'P', 'S', '2', 'F' )
 };
 
 }
