777dd85e06a2d31a195f5ca066f24da6e2a1fe57
Wunkolo <wunkolo@gmail.com>
2018-05-17 14:07:05 -0700

    Add structure packing enforcement

--- include/Maple2/Types.hpp
+++ include/Maple2/Types.hpp
@@ -7,13 +7,12 @@
 
 namespace Maple2
 {
-#pragma pack(1)
+#pragma pack( push, 1 )
 
 struct PackFileHeaderVer1 // MS2F
 {
-private:
-	std::uint32_t Unknown00;              // Always zero
 public:
+	std::uint32_t Pad;                    // Always zero
 
 	std::uint64_t FATCompressedSize;      // DEFLATE length
 	std::uint64_t FATEncodedSize;         // Base64 length
@@ -25,10 +24,16 @@ public:
 	std::uint64_t TotalFiles;             // Total Files
 	std::uint64_t FATSize;                // Uncompressed size
 };
+static_assert(
+	sizeof(PackFileHeaderVer1) == 0x3C,
+	"sizeof(PackFileHeaderVer1) != 0x3C"
+);
 
 struct PackFileHeaderVer2 // NS2F
 {
+public:
 	std::uint32_t TotalFiles;
+
 	std::uint64_t FATCompressedSize;      // DEFLATE length
 	std::uint64_t FATEncodedSize;         // Base64 length
 	std::uint64_t FATSize;                // Uncompressed size
@@ -37,19 +42,27 @@ struct PackFileHeaderVer2 // NS2F
 	std::uint64_t FileListEncodedSize;    // Base64 length
 	std::uint64_t FileListSize;           // Uncompressed size
 };
+static_assert(
+	sizeof(PackFileHeaderVer2) == 0x34,
+	"sizeof(PackFileHeaderVer2) != 0x34"
+);
 
 struct FATEntry
 {
-private:
-	std::uint32_t Pad;
 public:
+	std::uint32_t Pad;
 	std::uint32_t FileIndex;
-	std::uint64_t Unknown; // 0xEE000009
+	std::uint64_t Unknown;                 // 0xEE000009
 	std::uint64_t Offset;
 	std::uint64_t EncodedSize;
 	std::uint64_t CompressedSize;
 	std::uint64_t Size;
 };
+static_assert(
+	sizeof(FATEntry) == 0x30,
+	"sizeof(FATEntry) != 0x30"
+);
 
-#pragma pack()
+
+#pragma pack( pop )
 }
