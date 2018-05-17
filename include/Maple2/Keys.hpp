9cdb93746af2160c9e62c653b8deed78abe6a1ad
Wunkolo <wunkolo@gmail.com>
2018-05-17 00:05:00 -0700

    Add MS2F_XOR_Key

--- include/Maple2/Keys.hpp
+++ include/Maple2/Keys.hpp
@@ -7,6 +7,7 @@ namespace Maple2
 
 extern const std::uint8_t MS2F_Key_LUT[128][32];
 extern const std::uint8_t MS2F_IV_LUT[128][16];
+extern const std::uint8_t MS2F_XOR_Key[2048];
 
 extern const std::uint8_t NS2F_Key_LUT[128][32];
 extern const std::uint8_t NS2F_IV_LUT[128][16];
