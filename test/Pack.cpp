4ca1770d43f9f6331b022e321e1d71b106dcf1be
Wunkolo <wunkolo@gmail.com>
2018-05-28 23:39:19 -0700

    Draft Packfile iteration

--- test/Pack.cpp
+++ test/Pack.cpp
@@ -3,9 +3,19 @@
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <vector>
 
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
+
+#include <Maple2/Maple2.hpp>
+
+#include <Util/Util.hpp>
+
+bool PackFolder(
+	const fs::path& TargetFolder,
+	Maple2::Identifier PackVersion
+);
 
 int main( int argc, char* argv[] )
 {
@@ -23,5 +33,155 @@ int main( int argc, char* argv[] )
 		return EXIT_FAILURE;
 	}
 
+	for( std::size_t i = 1; i < static_cast<std::size_t>(argc); ++i )
+	{
+		PackFolder( argv[i], Maple2::Identifier::MS2F );
+	}
+
 	return EXIT_SUCCESS;
+}
+
+template< typename PackTraits >
+bool MakePackFile(
+	const fs::path& TargetFolder,
+	std::ofstream& HeaderFile,
+	std::ofstream& DataFile
+)
+{
+	typename PackTraits::StreamType StreamHeader = {};
+	std::vector<typename PackTraits::FileHeaderType> FileTable;
+
+	for( 
+		const auto& CurFile
+		: fs::recursive_directory_iterator(
+			TargetFolder,
+			fs::directory_options::follow_directory_symlink
+		)
+	)
+	{
+		const auto& CurPath = CurFile.path();
+		const fs::path RelativePath = fs::path(
+			CurPath.wstring().substr(
+				TargetFolder.wstring().size() + 1
+			)
+		);
+
+		std::printf(
+			"%ls\n",
+			RelativePath.wstring().c_str()
+		);
+
+		StreamHeader.TotalFiles++;
+	}
+
+	// Write header
+	Util::Write(HeaderFile, PackTraits::Magic);
+	Util::Write(HeaderFile, StreamHeader);
+
+	return true;
+}
+
+bool PackFolder(
+	const fs::path& TargetFolder,
+	Maple2::Identifier PackVersion
+)
+{
+	if( !fs::exists( TargetFolder ) )
+	{
+		std::printf(
+			"Folder \"%ls\" does not exist\n",
+			TargetFolder.wstring().c_str()
+		);
+		return false;
+	}
+
+	if( !fs::is_directory( TargetFolder ) )
+	{
+		std::printf(
+			"\"%ls\" is not a folder\n",
+			TargetFolder.wstring().c_str()
+		);
+		return false;
+	}
+
+	auto TargetFile = fs::absolute(TargetFolder).parent_path() / TargetFolder.stem();
+
+	std::ofstream HeaderFile;
+	HeaderFile.open(
+		TargetFile.replace_extension(L".m2h"),
+		std::ios::binary | std::ios::trunc
+	);
+	std::printf(
+		"Creating header file: %ls\n",
+		TargetFile.wstring().c_str()
+	);
+	if( !HeaderFile.good() )
+	{
+		std::printf(
+			"Error creating file: %ls\n",
+			TargetFile.wstring().c_str()
+		);
+		return false;
+	}
+
+
+	std::ofstream DataFile;
+	DataFile.open(
+		TargetFile.replace_extension(L".m2d"),
+		std::ios::binary | std::ios::trunc
+	);
+	std::printf(
+		"Creating data file: %ls\n",
+		TargetFile.wstring().c_str()
+	);
+	if( !DataFile.good() )
+	{
+		std::printf(
+			"Error creating file: %ls\n",
+			TargetFile.wstring().c_str()
+		);
+		return false;
+	}
+
+	switch( PackVersion )
+	{
+	case Maple2::Identifier::MS2F:
+	{
+		MakePackFile<Maple2::PackTraits::MS2F>(
+			TargetFolder,
+			HeaderFile,
+			DataFile
+		);
+		break;
+	}
+	case Maple2::Identifier::NS2F:
+	{
+		MakePackFile<Maple2::PackTraits::NS2F>(
+			TargetFolder,
+			HeaderFile,
+			DataFile
+		);
+		break;
+	}
+	case Maple2::Identifier::OS2F:
+	{
+		MakePackFile<Maple2::PackTraits::OS2F>(
+			TargetFolder,
+			HeaderFile,
+			DataFile
+		);
+		break;
+	}
+	case Maple2::Identifier::PS2F:
+	{
+		MakePackFile<Maple2::PackTraits::PS2F>(
+			TargetFolder,
+			HeaderFile,
+			DataFile
+		);
+		break;
+	}
+	}
+
+	return true;
 }
