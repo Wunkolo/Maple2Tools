9d8f4a7992cef38932a53f29e4a1f061a1179918
Wunkolo <Wunkolo@gmail.com>
2018-07-20 12:43:44 -0700

    Update PackStream argument format

--- tools/Pack.cpp
+++ tools/Pack.cpp
@@ -6,12 +6,12 @@
 #include <vector>
 #include <sstream>
 #include <cstring>
+#include <string>
 
 #include <experimental/filesystem>
 namespace fs = std::experimental::filesystem;
 
 #include <Maple2/Maple2.hpp>
-
 #include <Util/Util.hpp>
 
 bool PackFolder(
@@ -27,39 +27,59 @@ int main(int argc, char* argv[])
 		"\tof the original folder's name\n"
 		"Build Date: " __TIMESTAMP__ "\n"
 		"\t- wunkolo <wunkolo@gmail.com>\n"
-		"Usage: Pack (M2SF/N2SF/O2SF/P2SF) (List of folders to pack)\n"
+		"Usage:\n\tPack <[M/N/O/P]2SF>:<Folder path>\n\tCan specify multiple paths\n"
+		"Example:\n\tPack MS2F:Data/Textures O2SF:Data/Models\n"
 	);
-	if( argc < 3 )
+	if( argc < 2 )
 	{
 		std::puts("Invalid arguments");
 		return EXIT_FAILURE;
 	}
 
-	const Maple2::Identifier PackVersion = *reinterpret_cast<Maple2::Identifier*>(argv[1]);
-
-	switch( PackVersion )
+	for( std::size_t i = 1; i < static_cast<std::size_t>(argc); ++i )
 	{
-	case Maple2::Identifier::MS2F:
-	case Maple2::Identifier::NS2F:
-	case Maple2::Identifier::OS2F:
-	case Maple2::Identifier::PS2F:
-	{
-		for( std::size_t i = 2; i < static_cast<std::size_t>(argc); ++i )
+		const std::string CurArg(argv[i]);
+		const std::size_t Sep = CurArg.find_first_of(":");
+		if( Sep == std::string::npos )
 		{
-			PackFolder(argv[i], PackVersion);
+			std::puts("Invalid PackStream argument( did you forget a \':\'?)");
+			continue;
 		}
-		break;
-	}
-	default:
-	{
-		std::printf(
-			"Unknown PackStream version: \'%s\'",
-			argv[1]
+		const fs::path CurPath = CurArg.substr(Sep + 1);
+		const std::string CurPackVersion = CurArg.substr(
+			0,
+			Sep
 		);
-		return EXIT_FAILURE;
-	}
-	}
+		if( CurPackVersion.size() != 4 )
+		{
+			std::printf(
+				"Invalid PackStream identifier: \'%s\'",
+				CurPackVersion.c_str()
+			);
+		}
+		const Maple2::Identifier PackVersion
+			= *reinterpret_cast<const Maple2::Identifier*>(CurPackVersion.data());
 
+		switch( PackVersion )
+		{
+		case Maple2::Identifier::MS2F:
+		case Maple2::Identifier::NS2F:
+		case Maple2::Identifier::OS2F:
+		case Maple2::Identifier::PS2F:
+		{
+			const bool PackResult = PackFolder(CurPath, PackVersion);
+			break;
+		}
+		default:
+		{
+			std::printf(
+				"Unknown PackStream version: \'%s\'",
+				CurPackVersion.c_str()
+			);
+			continue;
+		}
+		}
+	}
 	return EXIT_SUCCESS;
 }
 
@@ -131,10 +151,10 @@ bool MakePackFile(
 
 		// Add to FileList
 		FileList
-			<< StreamHeader.TotalFiles
-			<< ','
-			<< RelativePath.string()
-			<< "\r\n";
+		<< StreamHeader.TotalFiles
+		<< ','
+		<< RelativePath.string()
+		<< "\r\n";
 	}
 
 	// Create encrypted filelist
@@ -257,41 +277,37 @@ bool PackFolder(
 	{
 	case Maple2::Identifier::MS2F:
 	{
-		MakePackFile<Maple2::PackTraits::MS2F>(
+		return MakePackFile<Maple2::PackTraits::MS2F>(
 			TargetFolder,
 			HeaderFile,
 			DataFile
 		);
-		break;
 	}
 	case Maple2::Identifier::NS2F:
 	{
-		MakePackFile<Maple2::PackTraits::NS2F>(
+		return MakePackFile<Maple2::PackTraits::NS2F>(
 			TargetFolder,
 			HeaderFile,
 			DataFile
 		);
-		break;
 	}
 	case Maple2::Identifier::OS2F:
 	{
-		MakePackFile<Maple2::PackTraits::OS2F>(
+		return MakePackFile<Maple2::PackTraits::OS2F>(
 			TargetFolder,
 			HeaderFile,
 			DataFile
 		);
-		break;
 	}
 	case Maple2::Identifier::PS2F:
 	{
-		MakePackFile<Maple2::PackTraits::PS2F>(
+		return MakePackFile<Maple2::PackTraits::PS2F>(
 			TargetFolder,
 			HeaderFile,
 			DataFile
 		);
-		break;
 	}
 	}
 
-	return true;
+	return false;
 }
